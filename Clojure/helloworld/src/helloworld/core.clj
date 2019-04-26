9e6f27f5b158fda646817accfb57adfb7fa590b5
Ashish Waghmare <ashishwaghmare@gmail.com>
2019-04-26 19:15:01 +0530

    fixing code

--- Clojure/helloworld/src/helloworld/core.clj
+++ Clojure/helloworld/src/helloworld/core.clj
@@ -2,20 +2,23 @@
   (:gen-class))
 
 (defn isPrime
-   "Check if prime"
- [number]
- 
+  "Check if prime"
+  [number]
+  (loop [i 2 mc(int(Math/sqrt number))]
+    (print number mc i (= 0 (mod number i)) "\n" )
+    (if (< i mc)
+      (recur (inc i) mc))
+  )
 )
 
 (defn prime
   "Main function for generating prime"
   [count]
-  (print (filter isPrime (range 100)))   
-)
+  (print (filter isPrime (range 100)) "Next \n")   
+) 
 
 (defn -main
   "I can say 'Hello World'."
   [& args]
   ( prime 10
   ))
-
